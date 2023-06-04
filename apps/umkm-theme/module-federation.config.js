2bb221e6dc1a66a33c563b08687d0f9546e7706d
naufalyukafi <naufalyukafi10@gmail.com>
2023-06-04 15:23:51 +0700

    fix: fixing module federation

--- apps/umkm-theme/module-federation.config.js
+++ apps/umkm-theme/module-federation.config.js
@@ -10,16 +10,22 @@ const moduleFederationConfig = {
   exposes: {
     './Module': './src/remote-entry.ts',
   },
-  shared: {
-    swr: {
-      singleton: true,
-    },
+  shared: (name, config) => {
+    if (name === 'swr') {
+      return false;
+    }
   },
   optimization: {
     sideEffects: false,
     minimize: true,
     minimizer: [
-      new TerserPlugin(), // Plugin untuk mengompresi dan meminify JavaScript
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false, // Menghapus komentar JavaScript
+          },
+        },
+      }),
       new CssMinimizerPlugin({ // Plugin untuk mengompresi dan meminify CSS
         minimizerOptions: {
           preset: [
@@ -34,7 +40,7 @@ const moduleFederationConfig = {
     splitChunks: {
       chunks: 'all',
       cacheGroups: {
-        defaultVendors: {
+        vendors: {
           test: /[\\/]node_modules[\\/]/,
           name: 'vendors',
           chunks: 'all',
